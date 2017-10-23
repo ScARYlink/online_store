990a10692c045ca83f3d170ef8128136e6dcbc83
ScARYlink <scarylink25@gmail.com>
2017-10-23 23:52:04 +0300

    connect array with routes in the class Router

--- Source Files/components/Router.php
+++ Source Files/components/Router.php
@@ -10,10 +10,12 @@ class Router {
     private $routes;
 
     public function __construct() {
-
+        $routesPath = ROOT.'/config/routes.php';
+        $this->routes = include($routesPath);
     }
 
     public function run() {
+        print_r($this->routes);
         echo "Class Router, method run";
     }
 }
