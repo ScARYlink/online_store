74375314062f65f1545f16e9811fecee4bf3e433
ScARYlink <scarylink25@gmail.com>
2017-10-24 17:11:09 +0300

    create new controllers

---
+++ source.files/controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ScARYlink
+ * Date: 24.10.2017
+ * Time: 17:07
+ */
+
+class ProductController {
+
+    public function actionList() {
+        return true;
+    }
+}
