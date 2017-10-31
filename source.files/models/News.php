2a07dc4733a30917a4167bddbc763ba7f09ecb22
ScARYlink <scarylink25@gmail.com>
2017-10-31 16:07:30 +0200

    add method for get the news by id

--- source.files/models/News.php
+++ source.files/models/News.php
@@ -11,10 +11,23 @@ class News {
     //возырвт одной новости по id
     public static function getNewsItemById($id) {
         //запрос к БД
+        $id = intval($id);
+
+        if ($id) {
+            $db = Db::getConnection();
+
+            $result = $db->query('SELECT * from news WHERE id='.$id);
+            $result->setFetchMode(PDO::FETCH_ASSOC);
+
+            $newsItem = $result->fetch();
+
+            return $newsItem;
+        }
     }
 
     //возврат массива с новостями
     public static function getNewsList() {
         //запрос к БД
+
     }
 }
