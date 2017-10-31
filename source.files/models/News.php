6a46235b41d541878c59659e2e5e3e7496ae83fb
ScARYlink <scarylink25@gmail.com>
2017-10-31 16:19:06 +0200

    add method for get the all news

--- source.files/models/News.php
+++ source.files/models/News.php
@@ -28,6 +28,23 @@ class News {
     //возврат массива с новостями
     public static function getNewsList() {
         //запрос к БД
+        $db = Db::getConnection();
 
+        $newsList = array();
+
+        $result = $db->query('SELECT id, title, date, short_content '
+        .'FROM news '
+        .'ORDER BY date DESC '
+        .'LIMIT 10');
+
+        $i = 0;
+        while ($row = $result->fetch()) {
+            $newsList[$i]['id'] = $row['id'];
+            $newsList[$i]['title'] = $row['title'];
+            $newsList[$i]['date'] = $row['date'];
+            $newsList[$i]['short_content'] = $row['short_content'];
+            $i++;
+        }
+        return $newsList;
     }
 }
