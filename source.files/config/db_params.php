e9c1323c19f5fa28cc374dd3d0faf67e8a8c268f
ScARYlink <scarylink25@gmail.com>
2017-10-31 15:54:11 +0200

    params connection with DB

---
+++ source.files/config/db_params.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ScARYlink
+ * Date: 31.10.2017
+ * Time: 15:52
+ */
+
+return array(
+    'host' => 'localhost',
+    'dbname' => 'mvc_site',
+    'user' => 'root',
+    'password' => '',
+);
