7ec7e9ad4cbd77ed4d8baef957a4c9db5034faa2
ScARYlink <scarylink25@gmail.com>
2017-11-01 23:43:56 +0200

    ;

--- source.files/components/Db.php
+++ source.files/components/Db.php
@@ -10,10 +10,10 @@ class Db {
 
     public static function getConnection() {
         $paramsPath = ROOT.'/config/db_params.php';
-        $params = include($paramsPath);
+        $params = include ($paramsPath);
 
         $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
-        $db = new PDO($dsn, $params['user'], $params['password']);
+        $db = new PDO($dsn, $params['user'], $params['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
         return $db;
     }
