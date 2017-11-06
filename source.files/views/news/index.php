c77927d2064a1cb81af5114058af02e042de1c9f
ScARYlink <scarylink25@gmail.com>
2017-11-06 23:07:25 +0200

    refactoring the site code

--- source.files/views/news/index.php
+++ source.files/views/news/index.php
@@ -1,42 +1,33 @@
 <!doctype html>
-<html lang="en" class="no-js">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>
-
-    <link rel="stylesheet" href="../../template/css/reset.css"> <!-- CSS reset -->
-	<link rel="stylesheet" href="../../template/css/style.css"> <!-- Gem style -->
-	<script src="../../template/js/modernizr.js"></script> <!-- Modernizr -->
+    <link rel="stylesheet" href="../../template/css/stylesheet.css">
+	<!--<script src="../../template/js/modernizr.js"></script>-->
 
 	<title>Online Store</title>
 </head>
 <body>
-<header style="height: 150px;">
-    <h1>Responsive Vertical Timeline</h1>
-</header>
+    <header>
+        <div class="logo"></div>
+        <div class="menu"></div>
+        <div class="user"></div>
+    </header>
 
-<section id="cd-timeline" class="cd-container">
+    <main>
+        <nav>
 
-    <?php foreach ($newsList as $newsItem):?>
-    <div class="cd-timeline-block">
-        <div class="cd-timeline-img cd-picture">
-            <img src="../../template/img/cd-icon-picture.svg" alt="Picture">
-        </div> <!-- cd-timeline-img -->
+        </nav>
+        <section>
 
-        <div class="cd-timeline-content">
-            <h2><a href="/news/<?php echo $newsItem['id'];?>" ><?php echo $newsItem['title'];?></a></h2>
-            <p><?php echo $newsItem['short_content'];?></p>
-            <a href="/news/<?php echo $newsItem['id'];?>" class="cd-read-more">Read more</a>
-            <span class="cd-date"><?php echo $newsItem['date'];?></span>
-        </div> <!-- cd-timeline-content -->
-    </div> <!-- cd-timeline-block -->
-    <?php endforeach;?>
+        </section>
+    </main>
 
+    <footer>
 
-</section> <!-- cd-timeline -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="../../template/js/main.js"></script> <!-- Resource jQuery -->
+    </footer>
+
 </body>
 </html>
