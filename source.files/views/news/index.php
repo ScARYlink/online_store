03df4b4aba1c9672bf0715e930a82832694bf5b0
ScARYlink <scarylink25@gmail.com>
2017-11-07 13:01:42 +0200

    create a header layout

--- source.files/views/news/index.php
+++ source.files/views/news/index.php
@@ -10,18 +10,42 @@
 	<title>Online Store</title>
 </head>
 <body>
+
     <header>
-        <div class="logo"></div>
-        <div class="menu"></div>
-        <div class="user"></div>
+        <div class="logo hat">
+            <div class="wrap-logo">
+                <a href="../../views/news/index.php">
+                    <img src="../../template/img/nosa.png" alt="Логотип NOSA store">
+                </a>
+            </div>
+        </div>
+        <div class="menu hat">
+            <div class="navigation">Главная</div>
+            <div class="navigation">Магазин</div>
+            <div class="navigation">Блог</div>
+            <div class="navigation">О магазине</div>
+            <div class="navigation">Контакты</div>
+        </div>
+        <div class="user hat">
+            <div class="client-info">Корзина</div>
+            <div class="client-info">Аккаунт</div>
+            <div class="client-info">Вход</div>
+        </div>
     </header>
 
     <main>
-        <nav>
-
-        </nav>
+        <div class="category">
+            <a href="#">123</a>
+            <a href="#">234</a>
+            <a href="#">345</a>
+        </div>
         <section>
-
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
         </section>
     </main>
 
