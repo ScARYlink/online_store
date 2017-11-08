c5d8ef3532ad72743e2bf63316c9287e81234ea4
ScARYlink <scarylink25@gmail.com>
2017-11-08 15:12:14 +0200

    separation header and footer

--- source.files/views/site/index.php
+++ source.files/views/site/index.php
@@ -1,39 +1,6 @@
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php include ROOT.'/views/layouts/header.php'; ?>
 
-    <link rel="stylesheet" href="../../template/css/stylesheet.css">
-	<!--<script src="../../template/js/modernizr.js"></script>-->
-
-	<title>Online Store</title>
-</head>
-<body>
-
-    <header>
-        <div class="logo hat">
-            <div class="wrap-logo">
-                <a href="/index.php">
-                    <img src="../../template/img/nosa.png" alt="Логотип NOSA store">
-                </a>
-            </div>
-        </div>
-        <div class="menu hat">
-            <div class="navigation"><a href="#">Главная</a></div>
-            <div class="navigation"><a href="#">Магазин</a></div>
-            <div class="navigation"><a href="#">Блог</a></div>
-            <div class="navigation"><a href="#">О магазине</a></div>
-            <div class="navigation"><a href="#">Контакты</a></div>
-        </div>
-        <div class="user hat">
-            <div class="client-info"><a href="#">Корзина</a></div>
-            <div class="client-info"><a href="#">Аккаунт</a></div>
-            <div class="client-info"><a href="#">Вход</a></div>
-        </div>
-    </header>
-
-    <main>
+<main>
         <div class="category">
             <a href="#">123</a>
             <a href="#">234</a>
@@ -48,10 +15,4 @@
         </div>
 
     </main>
-
-    <footer>
-
-    </footer>
-
-</body>
-</html>
+<?php include ROOT.'/views/layouts/footer.php'; ?>
