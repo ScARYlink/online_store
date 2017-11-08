ffda0df46ecb90e941f0cae273b1bdef8d8a6203
ScARYlink <scarylink25@gmail.com>
2017-11-08 14:55:47 +0200

    create product controller

--- source.files/config/routes.php
+++ source.files/config/routes.php
@@ -9,5 +9,6 @@
 return array(
     //'site/([a-z]+)/([0-9]+)' => 'site/view/$1/$2',
 
+    'product/([0-9]+)' => 'product/view/$1',
     '' =>'site/index',
 );
---
+++ source.files/controllers/ProductController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ScARYlink
+ * Date: 08.11.2017
+ * Time: 14:54
+ */
+
+class ProductController {
+    
+}
