c6fc590d1768bfdd1acd307ae76b1bc7598531ce
ScARYlink <scarylink25@gmail.com>
2017-11-09 22:54:53 +0200

    create footer, add counter

--- source.files/views/layouts/footer.php
+++ source.files/views/layouts/footer.php
@@ -1,5 +1,11 @@
-<footer>
+    <footer>
+        <div class="f-counter f-block">
+            <a href="#">
+                <img src="../../template/img/counter.gif" alt="counter">
+            </a>
+        </div>
 
+        <div class="f-copyright f-block">Do not copy!</div>
     </footer>
 
 </body>
