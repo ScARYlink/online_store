672eeb062a94ec30afb564464bd5f7848e43062f
ScARYlink <scarylink25@gmail.com>
2017-11-09 11:23:37 +0200

    call the method in SiteController

--- source.files/controllers/SiteController.php
+++ source.files/controllers/SiteController.php
@@ -6,9 +6,15 @@
  * Time: 14:39
  */
 
+include_once ROOT.'/models/Category.php';
+
 class SiteController {
 
     public function actionIndex() {
+
+        $categories = array();
+        $categories = Category::getCategoriesList();
+
         require_once(ROOT.'/views/site/index.php');
 
         return true;
